d0a5e9092377110b7a1205be90b31fa59f16d640
DarlanHenrique <darlan.silva@ice.ufjf.br>
2024-12-14 10:13:35 -0300

    adding logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import PRODUCTIONS from './Audiovisual_productions.json'
 import Production from './Production'
+import logo from './assets/logo/Net_Prime_Plus_Max_Flix.png';
 
 function App() {
 
   return (
     <div>
-      <h1>Produções Audiovisuais</h1>
+      <img className="logo" src={logo} alt="Net Prime Plus Max Flix" />
       {PRODUCTIONS.audiovisual_productions.map((production) => (
         <Production 
           key={production.name}
