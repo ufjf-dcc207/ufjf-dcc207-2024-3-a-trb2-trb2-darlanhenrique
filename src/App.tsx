bd9a57eda6715abdc849c7b295f1ce0b1a3892cf
DarlanHenrique <darlan.silva@ice.ufjf.br>
2024-12-14 09:18:09 -0300

    using map to get all Productions in Audiovisual_productions jon

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,19 @@ function App() {
   return (
     <div>
       <h1>Produções Audiovisuais</h1>
-      <Production 
-        name={PRODUCTIONS.audiovisual_productions[0].name} 
-        type={PRODUCTIONS.audiovisual_productions[0].type} 
-        genre={PRODUCTIONS.audiovisual_productions[0].genre} 
-        length={PRODUCTIONS.audiovisual_productions[0].length} 
-        description={PRODUCTIONS.audiovisual_productions[0].description} 
-        image={PRODUCTIONS.audiovisual_productions[0].image}
-        isNew={PRODUCTIONS.audiovisual_productions[0].isNew} 
-      />
-      <Production 
-        name={PRODUCTIONS.audiovisual_productions[1].name} 
-        type={PRODUCTIONS.audiovisual_productions[1].type} 
-        genre={PRODUCTIONS.audiovisual_productions[1].genre} 
-        length={PRODUCTIONS.audiovisual_productions[1].length} 
-        description={PRODUCTIONS.audiovisual_productions[1].description} 
-        image={PRODUCTIONS.audiovisual_productions[1].image}
-        isNew={PRODUCTIONS.audiovisual_productions[1].isNew}
-      />
+      {PRODUCTIONS.audiovisual_productions.map((production, index) => (
+        <Production 
+          key={index}
+          name={production.name} 
+          type={production.type} 
+          genre={production.genre} 
+          length={production.length} 
+          description={production.description} 
+          image={production.image}
+          isNew={production.isNew}
+        />
+      ))
+    }
     </div>
   )
 }
