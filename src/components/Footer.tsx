32f89a9323b252e3868d0fe0853d21aae3b34c58
DarlanHenrique <darlan.silva@ice.ufjf.br>
2025-01-07 03:55:41 -0300

    create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import "../css/Footer.css";
+import logo from '../assets/logo/Net_Prime_Plus_Max_Flix.png';
+
+export default function Footer() {
+    return (
+        <footer className="footer mt-5">
+            <div className="container">
+                <div className="">
+                    <div className="col-12 font-medium text-center text-gray-100 fs-4">
+                        <img className="logo" src={logo} alt="Disney Statz" />
+                    </div>
+                    <div className="col-12 font-medium text-center">
+                        <ul className="list-unstyled d-flex justify-content-center fs-6">
+                            <li className="me-3"><a href="#">Termos de uso</a></li>
+                            <li className="me-3"><a href="#">Política de privacidade</a></li>
+                            <li className="me-3"><a href="#">Contato</a></li>
+                            <li className="text-white">© 2024 - 2025 Disney Statz. Todos os direitos reservados.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    );
+
+}
--- src/site/index/App.tsx
+++ src/site/index/App.tsx
@@ -5,6 +5,7 @@ import PRODUCTIONS from '../../database/Audiovisual_productions.json'
 import Category from './Category';
 import Production from './Production.tsx'
 import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 const allGenres: string[] = Array.from(
   new Set(
@@ -27,56 +28,15 @@ function App() {
       <header className='mb-5'>
         <Navbar types={allTypes} genres={allGenres} />
       </header>
-      
-      <div className="top-space"></div>
 
-      {/* PARA AGRUPAR POR NOVAS PRODUÇÕES */}
-      <Category key="new-productions" name="Novas Produçõe">
-        {PRODUCTIONS.audiovisual_productions
-          .filter((production) => production.isNew)
-          .map((production) => (
-            <Production
-              key={production.name}
-              name={production.name}
-              year={production.year}
-              type={production.type}
-              genre={production.genre}
-              length={production.length}
-              description={production.description}
-              image={production.image}
-              isNew={production.isNew}
-              classification={production.indicativeClassification}
-              isOriginal={production.isOriginal}
-            />
-          ))}
-      </Category>
-      {/* PARA AGRUPAR POR GENEROS */}
-      {allGenres.map((genre) => (
-        <Category key={genre} type='category' name={genre}>
-          {PRODUCTIONS.audiovisual_productions.filter((production) =>
-            production.genre.map((g) =>
-              g.trim()).includes(genre)).map((production) => (
-                <Production
-                  key={production.name}
-                  name={production.name}
-                  year={production.year}
-                  type={production.type}
-                  genre={production.genre}
-                  length={production.length}
-                  description={production.description}
-                  image={production.image}
-                  isNew={production.isNew}
-                  classification={production.indicativeClassification}
-                  isOriginal={production.isOriginal}
-                />
-              ))}
-        </Category>
-      ))}
-      {/* PARA AGRUPAR POR TIPOS */}
-      {allTypes.map((type) => (
-        <Category key={type} name={type}>
-          {PRODUCTIONS.audiovisual_productions.filter((production) =>
-            production.type.trim() == type)
+
+      <div className="top-space"></div>
+      <main>
+
+        {/* PARA AGRUPAR POR NOVAS PRODUÇÕES */}
+        <Category key="new-productions" name="Novas Produçõe">
+          {PRODUCTIONS.audiovisual_productions
+            .filter((production) => production.isNew)
             .map((production) => (
               <Production
                 key={production.name}
@@ -93,9 +53,53 @@ function App() {
               />
             ))}
         </Category>
-      ))}
+        {/* PARA AGRUPAR POR GENEROS */}
+        {allGenres.map((genre) => (
+          <Category key={genre} type='category' name={genre}>
+            {PRODUCTIONS.audiovisual_productions.filter((production) =>
+              production.genre.map((g) =>
+                g.trim()).includes(genre)).map((production) => (
+                  <Production
+                    key={production.name}
+                    name={production.name}
+                    year={production.year}
+                    type={production.type}
+                    genre={production.genre}
+                    length={production.length}
+                    description={production.description}
+                    image={production.image}
+                    isNew={production.isNew}
+                    classification={production.indicativeClassification}
+                    isOriginal={production.isOriginal}
+                  />
+                ))}
+          </Category>
+        ))}
+        {/* PARA AGRUPAR POR TIPOS */}
+        {allTypes.map((type) => (
+          <Category key={type} name={type}>
+            {PRODUCTIONS.audiovisual_productions.filter((production) =>
+              production.type.trim() == type)
+              .map((production) => (
+                <Production
+                  key={production.name}
+                  name={production.name}
+                  year={production.year}
+                  type={production.type}
+                  genre={production.genre}
+                  length={production.length}
+                  description={production.description}
+                  image={production.image}
+                  isNew={production.isNew}
+                  classification={production.indicativeClassification}
+                  isOriginal={production.isOriginal}
+                />
+              ))}
+          </Category>
+        ))}
+      </main>
 
-
+      <Footer />
     </div>
   )
 }
